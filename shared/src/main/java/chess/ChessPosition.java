db44f189b1dca0b1556dbde9f3b31bfc2322b590
joonida <joon123121@hotmail.com>
2024-06-27 18:16:45 -0600

    implement(ChessPosition)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,22 +1,33 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessPosition {
-
-    public ChessPosition(int row, int col) {
+public class ChessPosition
+{
+    int row;
+    int column;
+    public ChessPosition(int row, int col)
+    {
+        //TESTS ARE IN 1 BASE INDEX
+        setRow(row);
+        setColumn(column);
     }
+
+    public void setRow(int rowIn) { row = rowIn; }
+    public void setColumn(int columnIn) { column = columnIn; }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +35,16 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return column;
     }
+
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition position = (ChessPosition) o;
+        return row == position.row && column == position.column;
+    }
+
+    public int hashCode() { return Objects.hash(row, column); }
 }
