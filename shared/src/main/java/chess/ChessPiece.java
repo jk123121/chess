6195ca7b10f83d0737cb67f5be1c97bf561c845a
joonida <joon123121@hotmail.com>
2024-07-08 17:54:58 -0600

    fix(formatting)

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -85,7 +85,8 @@ public class ChessPiece
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChessPiece that = (ChessPiece) o;
@@ -93,7 +94,8 @@ public class ChessPiece
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hash(type, color);
     }
 }
