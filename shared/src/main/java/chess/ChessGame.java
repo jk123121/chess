2bebfa9c5c87dfcadd9c798359aebcb857888c14
joonida <joon123121@hotmail.com>
2024-07-08 18:19:59 -0600

    implement(equals and hash)

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -103,4 +104,16 @@ public class ChessGame
      * @return the chessboard
      */
     public ChessBoard getBoard() { return board; }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return turn == chessGame.turn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(turn, board); }
 }
