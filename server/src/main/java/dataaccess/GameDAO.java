c9e612ee43baa57af78691dca294c154a96e7941
joonida <joon123121@hotmail.com>
2024-07-23 16:16:10 -0600

    format(toInterface)

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -5,74 +5,25 @@ import model.GameData;
 
 import java.util.ArrayList;
 
-public class GameDAO
+public interface GameDAO
 {
-    ArrayList<GameData> games = new ArrayList<>();
+    public int addGame(String whiteUsername, String blackUsername, String gameName, ChessGame game) throws DataAccessException;
 
-    public GameDAO() {}
+    public void endGame(int gameID) throws DataAccessException;
 
-    public int addGame(String whiteUsername, String blackUsername, String gameName, ChessGame game) throws DataAccessException
-    {
-        int gameID = games.size() + 1;
-        games.add(new GameData(gameID, whiteUsername, blackUsername, gameName, game));
-        return gameID;
-    }
+    public void delete() throws DataAccessException;
 
-    public void endGame(int gameID) throws DataAccessException
-    {
+    public void deleteAll() throws DataAccessException;
 
-    }
+    public String getWhiteUsername(int gameID) throws DataAccessException;
 
-    public void delete() throws DataAccessException
-    {
+    public void setWhiteUsername(int gameID, String username) throws DataAccessException;
 
-    }
+    public String getBlackUsername(int gameID) throws DataAccessException;
 
-    public void deleteAll() throws DataAccessException { games.clear(); }
+    public void setBlackUsername(int gameID, String username) throws DataAccessException;
 
-    public String getWhiteUsername(int gameID) throws DataAccessException
-    {
-        GameData game = find(gameID);
-        return game.getWhiteUsername();
-    }
+    public GameData find(int gameID) throws DataAccessException;
 
-    public void setWhiteUsername(int gameID, String username) throws DataAccessException
-    {
-        GameData game = find(gameID);
-        game.setWhiteUsername(username);
-    }
-
-    public String getBlackUsername(int gameID) throws DataAccessException
-    {
-        GameData game = find(gameID);
-        return game.getBlackUsername();
-    }
-
-    public void setBlackUsername(int gameID, String username) throws DataAccessException
-    {
-        GameData game = find(gameID);
-        game.setBlackUsername(username);
-    }
-
-    public GameData find(int gameID) throws DataAccessException
-    {
-        for (GameData game : games)
-        {
-            if (game.getGameID() == gameID)
-            {
-                return game;
-            }
-        }
-        return null;
-    }
-
-    public ArrayList<GameData> listGames ()
-    {
-        ArrayList<GameData> gameList = new ArrayList<>();
-        for (GameData game : games)
-        {
-            gameList.add(new GameData(game.getGameID(), game.getWhiteUsername(), game.getBlackUsername(), game.getGameName()));
-        }
-        return gameList;
-    }
+    public ArrayList<GameData> listGames ();
 }
