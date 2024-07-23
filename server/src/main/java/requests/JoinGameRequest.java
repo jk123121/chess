b9008e96ead26cbf948b1d7cf71a029e1b3febb3
joonida <joon123121@hotmail.com>
2024-07-23 17:06:48 -0600

    impl(JoinGameRequest)

--- server/src/main/java/requests/JoinGameRequest.java
+++ server/src/main/java/requests/JoinGameRequest.java
@@ -2,4 +2,23 @@ package requests;
 
 public class JoinGameRequest
 {
+    private String playerColor;
+    private int gameID;
+
+    public String getPlayerColor() { return playerColor; }
+    public void setPlayerColor(String playerColor) { this.playerColor = playerColor; }
+
+    public int getGameID() { return gameID; }
+    public void setGameID(int gameID) { this.gameID = gameID; }
+
+    public JoinGameRequest(String playerColor, int gameID)
+    {
+        setPlayerColor(playerColor);
+        setGameID(gameID);
+    }
+
+    public JoinGameRequest(String playerColor)
+    {
+        setPlayerColor(playerColor);
+    }
 }
