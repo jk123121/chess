0e00f01b47708cd271aa09e7dc34d844aea0f092
joonida <joon123121@hotmail.com>
2024-07-23 17:08:00 -0600

    impl(JoinGameService)

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -1,5 +1,56 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import requests.JoinGameRequest;
+import results.JoinGameResult;
+
 public class JoinGameService
 {
+    public JoinGameResult joinGame(JoinGameRequest request, String authToken, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        String color = request.getPlayerColor();
+        int gameID = request.getGameID();
+
+        try
+        {
+            String username = authDAO.getUser(authToken);
+
+            if (username == null) { return new JoinGameResult("Error: unauthorized"); }
+
+            if (gameDAO.find(gameID) == null)
+            {
+                return new JoinGameResult("Error: bad request");
+            }
+
+            if (color == null || color.equals("")) { return new JoinGameResult(null); }
+
+            if ((color.equals("WHITE") &&
+                    gameDAO.getWhiteUsername(gameID) != null &&
+                    !gameDAO.getWhiteUsername(gameID).equals(username))
+                    ||
+                    (color.equals("BLACK") &&
+                            gameDAO.getBlackUsername(gameID) != null &&
+                            !gameDAO.getBlackUsername(gameID).equals(username)))
+            {
+                return new JoinGameResult("Error: already taken");
+            }
+
+            if (color.equals("WHITE") &&
+                    (gameDAO.getWhiteUsername(gameID) == null || gameDAO.getWhiteUsername(gameID).equals(username)))
+            {
+                gameDAO.setWhiteUsername(gameID, username);
+            }
+            if (color.equals("BLACK") &&
+                    (gameDAO.getBlackUsername(gameID) == null || gameDAO.getBlackUsername(gameID).equals(username)))
+            {
+                gameDAO.setBlackUsername(gameID, username);
+            }
+            return new JoinGameResult(null);
+        } catch (DataAccessException e)
+        {
+            return new JoinGameResult(e.getMessage());
+        }
+    }
 }
