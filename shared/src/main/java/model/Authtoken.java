f247069de07fbe64d26ec1afb877f707a37c67fa
joonida <joon123121@hotmail.com>
2024-07-23 15:01:23 -0600

    implement(Authtoken)

--- shared/src/main/java/model/Authtoken.java
+++ shared/src/main/java/model/Authtoken.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.util.Objects;
+
 public class Authtoken
 {
+    private String authToken;
+    private String username;
+
+    public String getAuthToken() { return authToken; }
+    public void setAuthToken(String authToken) { this.authToken = authToken; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public Authtoken(String authToken, String username)
+    {
+        setAuthToken(authToken);
+        setUsername(username);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authtoken authtoken = (Authtoken) o;
+        return Objects.equals(authToken, authtoken.authToken) && Objects.equals(username, authtoken.username);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(authToken, username); }
 }
