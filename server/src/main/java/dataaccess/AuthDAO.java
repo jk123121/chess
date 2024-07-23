79a469205f7a931d20f840e5bc13dc83bf5e1173
joonida <joon123121@hotmail.com>
2024-07-23 16:15:19 -0600

    format(toInterface)

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -4,54 +4,15 @@ import model.Authtoken;
 
 import java.util.ArrayList;
 
-public class AuthDAO
+public interface AuthDAO
 {
-    ArrayList<Authtoken> authTokens = new ArrayList<>();
+    public void insert(Authtoken token) throws DataAccessException;
 
-    public AuthDAO() {}
+    public void delete(String username) throws DataAccessException;
 
-    public void insert(Authtoken token) throws DataAccessException
-    {
-        for (int i = 0; i < authTokens.size(); i++)
-        {
-            if (authTokens.get(i).getUsername().equals(token.getUsername()))
-            {
-                authTokens.set(i, token);
-                return;
-            }
-        }
-        authTokens.add(token);
-    }
+    public void deleteAll() throws DataAccessException;
 
-    public void delete(String username) throws DataAccessException
-    {
-        Authtoken token = find(username);
-        if (token != null) { authTokens.remove(token); }
-    }
+    public Authtoken find(String username) throws DataAccessException;
 
-    public void deleteAll() throws DataAccessException { authTokens.clear(); }
-
-    public Authtoken find(String username) throws DataAccessException
-    {
-        for (Authtoken token : authTokens)
-        {
-            if (token.getUsername().equals(username))
-            {
-                return token;
-            }
-        }
-        return null;
-    }
-
-    public String getUser(String authtoken) throws DataAccessException
-    {
-        for (Authtoken token : authTokens)
-        {
-            if (token.getAuthToken().equals(authtoken))
-            {
-                return token.getUsername();
-            }
-        }
-        return null;
-    }
+    public String getUser(String authtoken) throws DataAccessException;
 }
