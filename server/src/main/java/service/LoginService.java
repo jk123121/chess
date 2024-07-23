2e5be7e469ecbd7773bfa952a81db1020474b59f
joonida <joon123121@hotmail.com>
2024-07-23 16:52:30 -0600

    impl(LoginService)

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,5 +1,37 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import model.Authtoken;
+import model.User;
+import requests.LoginRequest;
+import results.LoginResult;
+
+import java.util.UUID;
+
 public class LoginService
 {
+    public LoginResult login (LoginRequest request, MemoryUserDAO userDAO, MemoryAuthDAO authDAO) throws DataAccessException {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        User user;
+        try
+        {
+            if (userDAO.find(username) != null)
+            {
+                user = userDAO.find(username);
+                if (user.getPassword().equals(password))
+                {
+                    UUID uuid = UUID.randomUUID();
+                    authDAO.insert(new Authtoken(uuid.toString(), username));
+                    return new LoginResult(username, uuid.toString(), null);
+                }
+            }
+            return new LoginResult(null, null, "Error: unauthorized");
+        } catch (DataAccessException e)
+        {
+            return new LoginResult(null, null, e.getMessage());
+        }
+    }
 }
