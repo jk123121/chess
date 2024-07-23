14a5bff286ef1c3fa226d27f1ee4523acc8fe97d
joonida <joon123121@hotmail.com>
2024-07-23 16:55:07 -0600

    impl(LogoutRequest)

--- server/src/main/java/requests/LogoutRequest.java
+++ server/src/main/java/requests/LogoutRequest.java
@@ -2,4 +2,13 @@ package requests;
 
 public class LogoutRequest
 {
+    String token;
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public LogoutRequest(String token)
+    {
+        setToken(token);
+    }
 }
