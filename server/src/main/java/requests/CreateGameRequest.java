c64173e71a7daa2a0771bc43c3c911ce459815c1
joonida <joon123121@hotmail.com>
2024-07-23 16:58:45 -0600

    impl(CreateGameRequest)

--- server/src/main/java/requests/CreateGameRequest.java
+++ server/src/main/java/requests/CreateGameRequest.java
@@ -2,4 +2,10 @@ package requests;
 
 public class CreateGameRequest
 {
+    private String gameName;
+
+    public String getGameName() { return gameName; }
+    public void setGameName(String gameName) { this.gameName = gameName; }
+
+    public CreateGameRequest(String gameName) { setGameName(gameName); }
 }
