752cbe2aa75e60f2102f827dd4dda930f31a977e
joonida <joon123121@hotmail.com>
2024-07-23 16:50:58 -0600

    impl(LoginResult)

--- server/src/main/java/results/LoginResult.java
+++ server/src/main/java/results/LoginResult.java
@@ -2,4 +2,23 @@ package results;
 
 public class LoginResult
 {
+    private String username;
+    private String authToken;
+    private String message;
+
+    public String getAuthToken() { return authToken; }
+    public void setAuthToken(String authToken) { this.authToken = authToken; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public LoginResult(String username, String authToken, String message)
+    {
+        setAuthToken(authToken);
+        setUsername(username);
+        setMessage(message);
+    }
 }
