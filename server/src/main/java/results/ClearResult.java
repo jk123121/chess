6ddac44df58dace5e01ba4694faa6ccbb2730350
joonida <joon123121@hotmail.com>
2024-07-23 16:42:35 -0600

    implement(ClearResult)

--- server/src/main/java/results/ClearResult.java
+++ server/src/main/java/results/ClearResult.java
@@ -2,4 +2,10 @@ package results;
 
 public class ClearResult
 {
+    private String message;
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public ClearResult(String message) { setMessage(message); }
 }
