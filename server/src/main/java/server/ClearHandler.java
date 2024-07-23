2f22e59a72ee4b06040e5c406785920b3388b5a5
joonida <joon123121@hotmail.com>
2024-07-23 17:11:07 -0600

    impl(ClearHandler)

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -1,5 +1,27 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import results.ClearResult;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+
 public class ClearHandler
 {
+    public Object handle(Request req, Response res, MemoryUserDAO userDAO, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        Gson gson = new Gson();
+        ClearResult result = new ClearService().clear(userDAO, authDAO, gameDAO);
+
+        if (result.getMessage() == null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
