44e2e0d26012ce59ea4f4cfc29e49048b4035b1e
joonida <joon123121@hotmail.com>
2024-07-23 16:48:04 -0600

    impl(RegisterResult)

--- server/src/main/java/results/RegisterResult.java
+++ server/src/main/java/results/RegisterResult.java
@@ -2,4 +2,23 @@ package results;
 
 public class RegisterResult
 {
+    private String username;
+    private String authToken;
+    private String message;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getAuthToken() { return authToken; }
+    public void setAuthToken(String authToken) { this.authToken = authToken; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public RegisterResult(String username, String authToken, String message)
+    {
+        setUsername(username);
+        setAuthToken(authToken);
+        setMessage(message);
+    }
 }
