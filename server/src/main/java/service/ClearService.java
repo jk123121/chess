1c38bdd590565fe9dbfebf30b8f2f95a8f93ff19
joonida <joon123121@hotmail.com>
2024-07-23 16:43:42 -0600

    implement(ClearService)

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,5 +1,24 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import results.ClearResult;
+
 public class ClearService
 {
+    public ClearResult clear(MemoryUserDAO userDAO, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        try
+        {
+            userDAO.deleteAll();
+            authDAO.deleteAll();
+            gameDAO.deleteAll();
+            return new ClearResult(null);
+        } catch (DataAccessException e)
+        {
+            return new ClearResult(e.getMessage());
+        }
+    }
 }
