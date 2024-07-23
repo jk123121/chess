b836646ade38f8762a8eec17a7cec9e53f9814ee
joonida <joon123121@hotmail.com>
2024-07-23 16:57:53 -0600

    impl(CreateGameResult)

--- server/src/main/java/results/CreateGameResult.java
+++ server/src/main/java/results/CreateGameResult.java
@@ -2,4 +2,23 @@ package results;
 
 public class CreateGameResult
 {
+    private Integer gameID;
+    private String message;
+
+    public int getGameID() { return gameID; }
+    public void setGameID(Integer gameID) { this.gameID = gameID; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public CreateGameResult(Integer gameID, String message)
+    {
+        setGameID(gameID);
+        setMessage(message);
+    }
+
+    public CreateGameResult(String message)
+    {
+        setMessage(message);
+    }
 }
