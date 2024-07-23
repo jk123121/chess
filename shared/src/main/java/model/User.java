d3151ffc7a665681eee28b27106e40720135fc8f
joonida <joon123121@hotmail.com>
2024-07-23 15:00:17 -0600

    add(equals and hashcode)

--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class User
 {
     private String username;
@@ -20,4 +22,16 @@ public class User
         setPassword(password);
         setEmail(email);
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(username, password, email); }
 }
