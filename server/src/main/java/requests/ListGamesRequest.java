8fab609d499017e2ba0918295ce1675bea07780f
joonida <joon123121@hotmail.com>
2024-07-23 17:03:14 -0600

    impl(ListGameRequest)

--- server/src/main/java/requests/ListGamesRequest.java
+++ server/src/main/java/requests/ListGamesRequest.java
@@ -2,4 +2,10 @@ package requests;
 
 public class ListGamesRequest
 {
+    boolean success;
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+
+    public ListGamesRequest(boolean success) { setSuccess(success); }
 }
