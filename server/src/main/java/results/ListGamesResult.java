7fc8680e6956f3865bf9dac0e8f3036cfd14bc6f
joonida <joon123121@hotmail.com>
2024-07-23 17:02:45 -0600

    impl(ListGameResult)

--- server/src/main/java/results/ListGamesResult.java
+++ server/src/main/java/results/ListGamesResult.java
@@ -1,5 +1,28 @@
 package results;
 
+import model.GameData;
+import java.util.ArrayList;
+
 public class ListGamesResult
 {
+    ArrayList<GameData> games;
+    private String message;
+
+    public ArrayList<GameData> getGames() { return games; }
+
+    public void setGames(ArrayList<GameData> games) { this.games = games; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public ListGamesResult(ArrayList<GameData> games, String message)
+    {
+        setGames(games);
+        setMessage(message);
+    }
+
+    public ListGamesResult(String message)
+    {
+        setMessage(message);
+    }
 }
