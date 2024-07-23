a00d29750b817e4d8aa559f01a792c210a3660b2
joonida <joon123121@hotmail.com>
2024-07-23 16:59:50 -0600

    impl(CreateGameService)

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,5 +1,36 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import requests.CreateGameRequest;
+import results.CreateGameResult;
+
 public class CreateGameService
 {
+    public CreateGameResult createGame(CreateGameRequest request, String authToken, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        String gameName = request.getGameName();
+
+        try
+        {
+            if (authDAO.getUser(authToken) != null)
+            {
+                if (gameName == null || gameName.equals(""))
+                {
+                    return new CreateGameResult("Error: bad request");
+                }
+
+                ChessGame game = new ChessGame();
+                game.getBoard().resetBoard();
+                Integer gameID = gameDAO.addGame(null, null, gameName, game);
+                return new CreateGameResult(gameID, null);
+            }
+            return new CreateGameResult(null, "Error: unauthorized");
+        } catch (DataAccessException e)
+        {
+            return new CreateGameResult(null, e.getMessage());
+        }
+    }
 }
