9d9a71b1ce1e70ca8e07dfa1f498408e11fc0d94
joonida <joon123121@hotmail.com>
2024-07-23 16:55:47 -0600

    impl(LogoutService)

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,5 +1,27 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import requests.LogoutRequest;
+import results.LogoutResult;
+
 public class LogoutService
 {
+    public LogoutResult logout(LogoutRequest request, MemoryAuthDAO authDAO) throws DataAccessException {
+        String authtoken = request.getToken();
+
+        try
+        {
+            String username = authDAO.getUser(authtoken);
+            if (username != null)
+            {
+                authDAO.delete(username);
+                return new LogoutResult(null);
+            }
+            return new LogoutResult("Error: unauthorized");
+        } catch (DataAccessException e)
+        {
+            return new LogoutResult(e.getMessage());
+        }
+    }
 }
