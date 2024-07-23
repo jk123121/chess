c78e9050d5776484a938c3be30d6a2b9f58b24d9
joonida <joon123121@hotmail.com>
2024-07-23 17:06:07 -0600

    impl(JoinGameResult)

--- server/src/main/java/results/JoinGameResult.java
+++ server/src/main/java/results/JoinGameResult.java
@@ -2,4 +2,13 @@ package results;
 
 public class JoinGameResult
 {
+    private String message;
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public JoinGameResult(String message)
+    {
+        setMessage(message);
+    }
 }
