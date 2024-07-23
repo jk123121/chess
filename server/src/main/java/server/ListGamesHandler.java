0d3b4bfe1d3a8826095d9a0d17fc3a1d70ca009a
joonida <joon123121@hotmail.com>
2024-07-23 17:16:38 -0600

    impl(ListGamesHandler)

--- server/src/main/java/server/ListGamesHandler.java
+++ server/src/main/java/server/ListGamesHandler.java
@@ -1,5 +1,32 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import results.ListGamesResult;
+import service.ListGamesService;
+import spark.Request;
+import spark.Response;
+
 public class ListGamesHandler
 {
+    public Object handle(Request req, Response res, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        Gson gson = new Gson();
+        String authToken = req.headers("authorization");
+        ListGamesResult result = new ListGamesService().listGames(authToken, authDAO, gameDAO);
+
+        if (result.getMessage() == null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        if (result.getMessage() == "Error: unauthorized")
+        {
+            res.status(401);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
