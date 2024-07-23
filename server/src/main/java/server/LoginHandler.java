5cac485e5ece3f6d7c762335528eab032cad74c8
joonida <joon123121@hotmail.com>
2024-07-23 17:14:24 -0600

    impl(LoginHandler)

--- server/src/main/java/server/LoginHandler.java
+++ server/src/main/java/server/LoginHandler.java
@@ -1,5 +1,34 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import requests.LoginRequest;
+import results.LoginResult;
+import service.LoginService;
+import spark.Request;
+import spark.Response;
+
 public class LoginHandler
 {
+    public Object handle(Request req, Response res, MemoryUserDAO userDAO, MemoryAuthDAO authDAO) throws DataAccessException
+    {
+        Gson gson = new Gson();
+        LoginRequest request = gson.fromJson(req.body(), LoginRequest.class);
+        LoginResult result = new LoginService().login(request, userDAO, authDAO);
+
+        if (result.getMessage() == "Error: unauthorized")
+        {
+            res.status(401);
+            return gson.toJson(result);
+        }
+        if (result.getAuthToken() != null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
