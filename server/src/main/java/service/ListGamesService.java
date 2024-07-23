a362f7c3f3440d63b5db8ab1cec5c728d1a98630
joonida <joon123121@hotmail.com>
2024-07-23 17:03:59 -0600

    impl(ListGamesService)

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,5 +1,24 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import results.ListGamesResult;
+
 public class ListGamesService
 {
+    public ListGamesResult listGames(String authToken, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO)
+    {
+        try
+        {
+            if (authDAO.getUser(authToken) != null)
+            {
+                return new ListGamesResult(gameDAO.listGames(), null);
+            }
+            return new ListGamesResult("Error: unauthorized");
+        } catch (DataAccessException e)
+        {
+            return new ListGamesResult(e.getMessage());
+        }
+    }
 }
