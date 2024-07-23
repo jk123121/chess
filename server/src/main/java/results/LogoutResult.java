a9e7c2f15c8d67a1aa0147a1da850b0aede95846
joonida <joon123121@hotmail.com>
2024-07-23 16:54:21 -0600

    impl(LogoutResult)

--- server/src/main/java/results/LogoutResult.java
+++ server/src/main/java/results/LogoutResult.java
@@ -2,4 +2,10 @@ package results;
 
 public class LogoutResult
 {
+    private String message;
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public LogoutResult(String message) { setMessage(message); }
 }
