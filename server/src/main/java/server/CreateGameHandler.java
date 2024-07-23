76a4504c46415a488fd1aa5105b686593ae29861
joonida <joon123121@hotmail.com>
2024-07-23 17:15:46 -0600

    impl(CreateGameHandler)

--- server/src/main/java/server/CreateGameHandler.java
+++ server/src/main/java/server/CreateGameHandler.java
@@ -1,5 +1,39 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import requests.CreateGameRequest;
+import results.CreateGameResult;
+import service.CreateGameService;
+import spark.Request;
+import spark.Response;
+
 public class CreateGameHandler
 {
+    public Object handle(Request req, Response res, MemoryAuthDAO authDAO, MemoryGameDAO gameDAO) throws DataAccessException {
+        Gson gson = new Gson();
+        CreateGameRequest request = gson.fromJson(req.body(), CreateGameRequest.class);
+        String authToken = req.headers("authorization");
+        CreateGameResult result = new CreateGameService().createGame(request, authToken, authDAO, gameDAO);
+
+        if (result.getMessage() == null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        if (result.getMessage() == "Error: bad request")
+        {
+            res.status(400);
+            return gson.toJson(result);
+        }
+        if (result.getMessage() == "Error: unauthorized")
+        {
+            res.status(401);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
