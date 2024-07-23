7243b011513251fb922d3150055a61000e9e77ee
joonida <joon123121@hotmail.com>
2024-07-23 17:24:20 -0600

    impl(Server)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,8 @@
 package server;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
 import spark.*;
 
 public class Server
@@ -7,22 +10,49 @@ public class Server
 
     public int run(int desiredPort)
     {
-        Spark.port(desiredPort);
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
 
-        Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        try
+        {
+            Spark.port(desiredPort);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+            Spark.staticFiles.location("web");
 
-        Spark.awaitInitialization();
-        return Spark.port();
+            // Register your endpoints and handle exceptions here.
+            createRoutes(authDAO, userDAO, gameDAO);
+
+            //This line initializes the server and can be removed once you have a functioning endpoint
+            //Spark.init();
+
+            Spark.awaitInitialization();
+            return Spark.port();
+        }
+        catch(ArrayIndexOutOfBoundsException | NumberFormatException ex)
+        {
+            System.err.println("Specify the port number as a command line parameter");
+        }
+
+        return 0;
     }
 
     public void stop()
     {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private static void createRoutes(MemoryAuthDAO authDAO, MemoryUserDAO userDAO, MemoryGameDAO gameDAO)
+    {
+        Spark.get("/", (req, res) -> null);
+        Spark.post("/user", (req, res) -> new RegisterHandler().handle(req, res, userDAO, authDAO));
+        Spark.post("/session", (req, res) -> new LoginHandler().handle(req, res, userDAO, authDAO));
+        Spark.delete("/session", (req, res) -> new LogoutHandler().handle(req, res, authDAO));
+        Spark.delete("/db", (req, res) -> new ClearHandler().handle(req, res, userDAO, authDAO, gameDAO));
+        Spark.get("/game", (req, res) -> new ListGamesHandler().handle(req, res, authDAO, gameDAO));
+        Spark.post("/game", (req, res) -> new CreateGameHandler().handle(req, res, authDAO, gameDAO));
+        Spark.put("/game", (req, res) -> new JoinGameHandler().handle(req, res, authDAO, gameDAO));
     }
 }
