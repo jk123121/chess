5df98d2f5efadd11e43fb07fc71994d0db480ab5
joonida <joon123121@hotmail.com>
2024-07-23 16:16:27 -0600

    implement(MemoryUserDAO)

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,36 @@
 package dataaccess;
 
-public class MemoryUserDAO
+import model.User;
+
+import java.util.ArrayList;
+
+public class MemoryUserDAO implements UserDAO
 {
+    ArrayList<User> users = new ArrayList<>();
+
+    public MemoryUserDAO() {}
+
+    public void insert(User user) throws DataAccessException
+    {
+        users.add(user);
+    }
+
+    public void delete() throws DataAccessException
+    {
+
+    }
+
+    public void deleteAll() throws DataAccessException { users.clear(); }
+
+    public User find(String username) throws DataAccessException
+    {
+        for (User user : users)
+        {
+            if (user.getUsername().equals(username))
+            {
+                return user;
+            }
+        }
+        return null;
+    }
 }
