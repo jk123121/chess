6d77779710b15afd4b498f8afc7b14e509ad0520
joonida <joon123121@hotmail.com>
2024-07-23 17:14:57 -0600

    impl(LogoutHandler)

--- server/src/main/java/server/LogoutHandler.java
+++ server/src/main/java/server/LogoutHandler.java
@@ -1,5 +1,34 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import requests.LogoutRequest;
+import results.LogoutResult;
+import service.LogoutService;
+import spark.Request;
+import spark.Response;
+
 public class LogoutHandler
 {
+    public Object handle(Request req, Response res, MemoryAuthDAO authDAO) throws DataAccessException
+    {
+        Gson gson = new Gson();
+        String authToken = req.headers("authorization");
+        LogoutRequest request = new LogoutRequest(authToken);
+        LogoutResult result = new LogoutService().logout(request, authDAO);
+
+        if (result.getMessage() == null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        if (result.getMessage() == "Error: unauthorized")
+        {
+            res.status(401);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
