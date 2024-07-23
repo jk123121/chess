62225cfc8696643a3ab0aec360a7dc18e656fa00
joonida <joon123121@hotmail.com>
2024-07-23 16:51:33 -0600

    impl(LoginRequest)

--- server/src/main/java/requests/LoginRequest.java
+++ server/src/main/java/requests/LoginRequest.java
@@ -2,4 +2,18 @@ package requests;
 
 public class LoginRequest
 {
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public LoginRequest(String username, String password)
+    {
+        setUsername(username);
+        setPassword(password);
+    }
 }
