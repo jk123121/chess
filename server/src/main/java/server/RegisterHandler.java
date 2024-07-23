5d5ab297a56e30181446cb216ecc86ff49e8a5f8
joonida <joon123121@hotmail.com>
2024-07-23 17:13:29 -0600

    impl(RegisterHandler)

--- server/src/main/java/server/RegisterHandler.java
+++ server/src/main/java/server/RegisterHandler.java
@@ -1,5 +1,38 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import requests.RegisterRequest;
+import results.RegisterResult;
+import service.RegisterService;
+import spark.Request;
+import spark.Response;
+
 public class RegisterHandler
 {
+    public Object handle(Request req, Response res, MemoryUserDAO userDAO, MemoryAuthDAO authDAO)
+    {
+        Gson gson = new Gson();
+        RegisterRequest request = gson.fromJson(req.body(), RegisterRequest.class);
+        RegisterResult result = new RegisterService().register(request, userDAO, authDAO);
+
+        if (result.getMessage() == "Error: already taken")
+        {
+            res.status(403);
+            return gson.toJson(result);
+        }
+        if (result.getMessage() == "Error: bad request")
+        {
+            res.status(400);
+            return gson.toJson(result);
+        }
+        if (result.getAuthToken() != null)
+        {
+            res.status(200);
+            return gson.toJson(result);
+        }
+        res.status(500);
+        return gson.toJson(result);
+    }
 }
