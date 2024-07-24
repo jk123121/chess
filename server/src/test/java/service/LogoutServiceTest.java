260a03fc2f517ecd620ade0434fe81602d5e763b
joonida <joon123121@hotmail.com>
2024-07-23 23:03:34 -0600

    impl(LogoutServiceTest)

--- server/src/test/java/service/LogoutServiceTest.java
+++ server/src/test/java/service/LogoutServiceTest.java
@@ -1,5 +1,57 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.Authtoken;
+import model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.LogoutRequest;
+import results.LogoutResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LogoutServiceTest
 {
+    private MemoryUserDAO uDAO = new MemoryUserDAO();
+    private MemoryAuthDAO aDAO = new MemoryAuthDAO();
+    private MemoryGameDAO gDAO = new MemoryGameDAO();
+    private LogoutRequest request = new LogoutRequest("1234");
+
+    @BeforeEach
+    void setUp() throws DataAccessException
+    {
+        uDAO.insert(new User("ash", "pika", "chu"));
+        uDAO.insert(new User("notx", "pika", "chu"));
+        //Add that user to authtokens + generate authtoken
+        aDAO.insert(new Authtoken("1234", "ash"));
+        aDAO.insert(new Authtoken("1235", "notx"));
+        //Add new game
+        gDAO.addGame("ash", "notx", "demoGame", new ChessGame());
+    }
+
+    @Test
+    void positiveLogoutTest() throws DataAccessException
+    {
+        LogoutResult result = new LogoutService().logout(request, aDAO);
+        assertEquals(null, result.getMessage());
+    }
+
+    @Test
+    void negativeLogoutTest() throws DataAccessException
+    {
+        request = new LogoutRequest("1236");
+        LogoutResult result = new LogoutService().logout(request, aDAO);
+        assertEquals("Error: unauthorized", result.getMessage());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        new ClearService().clear(uDAO, aDAO, gDAO);
+    }
 }
