07ee0dc5128f980df0af839d93dbce496d4a017e
joonida <joon123121@hotmail.com>
2024-07-23 23:00:31 -0600

    impl(RegisterServiceTest)

--- server/src/test/java/service/RegisterServiceTest.java
+++ server/src/test/java/service/RegisterServiceTest.java
@@ -1,5 +1,57 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.Authtoken;
+import model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.RegisterRequest;
+import results.RegisterResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RegisterServiceTest
 {
+    private MemoryUserDAO uDAO = new MemoryUserDAO();
+    private MemoryAuthDAO aDAO = new MemoryAuthDAO();
+    private MemoryGameDAO gDAO = new MemoryGameDAO();
+    private RegisterRequest request = new RegisterRequest("noty", "notypassword", "noty@noty.com");
+
+    @BeforeEach
+    void setUp() throws DataAccessException
+    {
+        uDAO.insert(new User("ash", "pika", "chu"));
+        uDAO.insert(new User("notx", "pika", "chu"));
+        //Add that user to authtokens + generate authtoken
+        aDAO.insert(new Authtoken("1234", "ash"));
+        aDAO.insert(new Authtoken("1235", "notx"));
+        //Add new game
+        gDAO.addGame("ash", "notx", "demoGame", new ChessGame());
+    }
+
+    @Test
+    void positiveRegisterTest()
+    {
+        RegisterResult result = new RegisterService().register(request, uDAO, aDAO);
+        assertEquals("noty", result.getUsername());
+    }
+
+    @Test
+    void negativeRegisterTest()
+    {
+        request = new RegisterRequest("ash", "pika", "chu");
+        RegisterResult result = new RegisterService().register(request, uDAO, aDAO);
+        assertEquals("Error: already taken", result.getMessage());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        new ClearService().clear(uDAO, aDAO, gDAO);
+    }
 }
