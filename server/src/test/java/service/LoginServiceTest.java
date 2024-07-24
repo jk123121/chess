72b86f53631842dffae009b61530ef145ae7b416
joonida <joon123121@hotmail.com>
2024-07-23 23:02:10 -0600

    impl(LoginServiceTest)

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -1,5 +1,57 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.Authtoken;
+import model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.LoginRequest;
+import results.LoginResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LoginServiceTest
 {
+    private MemoryUserDAO uDAO = new MemoryUserDAO();
+    private MemoryAuthDAO aDAO = new MemoryAuthDAO();
+    private MemoryGameDAO gDAO = new MemoryGameDAO();
+    private LoginRequest request = new LoginRequest("ash", "pika");
+
+    @BeforeEach
+    void setUp() throws DataAccessException
+    {
+        uDAO.insert(new User("ash", "pika", "chu"));
+        uDAO.insert(new User("notx", "pika", "chu"));
+        //Add that user to authtokens + generate authtoken
+        aDAO.insert(new Authtoken("1234", "ash"));
+        aDAO.insert(new Authtoken("1235", "notx"));
+        //Add new game
+        gDAO.addGame("ash", "notx", "demoGame", new ChessGame());
+    }
+
+    @Test
+    void positiveLoginTest() throws DataAccessException
+    {
+        LoginResult result = new LoginService().login(request, uDAO, aDAO);
+        assertEquals("ash", result.getUsername());
+    }
+
+    @Test
+    void negativeLoginTest() throws DataAccessException
+    {
+        request = new LoginRequest("ash", "pikachu");
+        LoginResult result = new LoginService().login(request, uDAO, aDAO);
+        assertEquals("Error: unauthorized", result.getMessage());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        new ClearService().clear(uDAO, aDAO, gDAO);
+    }
 }
