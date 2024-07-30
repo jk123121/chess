1af58f07b9f68b58ea59ddcb603a9db2444ef30a
joonida <joon123121@hotmail.com>
2024-07-30 17:45:46 -0600

    impl(DBAuthDAO)

--- server/src/main/java/dataaccess/DBAuthDAO.java
+++ server/src/main/java/dataaccess/DBAuthDAO.java
@@ -2,35 +2,122 @@ package dataaccess;
 
 import model.Authtoken;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBAuthDAO implements AuthDAO
 {
     @Override
     public void insert(Authtoken token) throws DataAccessException
     {
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "one2three!@!M"))
+        {
+            conn.setCatalog("chess");
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO chess.authtoken (username, authtoken) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS))
+            {
+                preparedStatement.setString(1, token.getUsername());
+                preparedStatement.setString(2, token.getAuthToken());
 
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(String username) throws DataAccessException
     {
+        Authtoken token = find(username);
+        if (token != null)
+        {
+            try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "one2three!@!M"))
+            {
+                conn.setCatalog("chess");
 
+                try (var preparedStatement = conn.prepareStatement("DELETE FROM chess.authtoken WHERE username=? AND authtoken=?"))
+                {
+                    preparedStatement.setString(1, username);
+                    preparedStatement.setString(2, token.getAuthToken());
+                    preparedStatement.executeUpdate();
+                }
+            } catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
     public void deleteAll() throws DataAccessException
     {
-
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "one2three!@!M"))
+        {
+            conn.setCatalog("chess");
+            try (var preparedStatement = conn.prepareStatement("DELETE FROM chess.authtoken"))
+            {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Authtoken find(String username) throws DataAccessException
     {
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "one2three!@!M"))
+        {
+            conn.setCatalog("chess");
+
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM chess.authtoken WHERE username=?"))
+            {
+                preparedStatement.setString(1, username);
+
+                try (var result = preparedStatement.executeQuery())
+                {
+                    if (result.next())
+                    {
+                        //int id = result.getInt("id");
+                        return new Authtoken(result.getString("username"), result.getString("authtoken"));
+                    }
+                }
+            }
+
+        } catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public String getUser(String authtoken) throws DataAccessException
     {
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "one2three!@!M"))
+        {
+            conn.setCatalog("chess");
+
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM chess.authtoken WHERE authtoken=?"))
+            {
+                preparedStatement.setString(1, authtoken);
+
+                try (var result = preparedStatement.executeQuery())
+                {
+                    if (result.next())
+                    {
+                        //int id = result.getInt("id");
+                        return result.getString("username");
+                    }
+                }
+            }
+
+        } catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
         return "";
     }
 }
