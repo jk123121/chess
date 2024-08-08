e98b9f92f931751bba860a4be3819b84c4517098
joonida <joon123121@hotmail.com>
2024-08-07 18:18:41 -0600

    change(prettyprint)

--- client/src/main/java/facade/ServerFacade.java
+++ client/src/main/java/facade/ServerFacade.java
@@ -106,6 +106,7 @@ public class ServerFacade
     private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException
     {
         var status = http.getResponseCode();
+
         if (!isSuccessful(status))
         {
             throw new ResponseException(status, "failure: " + status);
